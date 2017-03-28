daeb8965ccbbd59242c74bed594acbf1ce8e9ed8
xdimh <xdimhf@163.com>
2017-03-28 11:47:33 +0800

    添加自动化提交构建任务

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,67 @@
 
 'use strict';
 const gulp = require('gulp');
+const shell = require('shelljs');
+const yargs = require('yargs');
 const gulpLoadPlugins = require('gulp-load-plugins');
 
 const $ = gulpLoadPlugins();
 
-// Publishes the site to GitHub Pages
-gulp.task('publish', () => {
+// 推送到github - weekly - gh-pages上
+gulp.task('publish.github',['build'],() => {
     console.log('Publishing to GH Pages');
 return gulp.src(['./_book/**/*','CNAME'])
     .pipe($.ghPages({
+        cacheDir:'publish-github',
         origin: 'origin',
         branch: 'gh-pages'
     }));
 });
+
+
+// 推送到coding.net weely.coding.me 上
+gulp.task('publish.coding.net',['build'],() => {
+    console.log('Publishing to coding.net Pages');
+    return gulp.src(['./_book/**/*'])
+        .pipe($.ghPages({
+            remoteUrl: 'https://git.coding.net/weekly/weekly.coding.me.git',
+            cacheDir : 'publish-coding-net',
+            branch: 'master',
+            force : true
+        }));
+});
+
+//build gitbook 为静态html
+gulp.task('build',(cb) => {
+    shell.exec('gitbook build', function(code, stdout, stderr) {
+        if (code == 0) {
+           cb();
+        } else {
+           throw new Error('gitbook build 失败!');
+        }
+    });
+});
+
+gulp.task('push-master',()=>{
+    let argv = yargs.usage('Usage: gulp push-master [options]')
+        .example('gulp push-master -m message')
+        .options({
+            m: {
+                alias: 'msg',
+                demand: true,
+                describe: 'commit message',
+                type: 'string'
+            }
+        })
+        .help('h')
+        .alias('h', 'help')
+        .epilog('copyright 2017')
+        .argv;
+    return gulp.src([__dirname + '/**','!' + __dirname + '/_book/**','!' + __dirname + '/*publish*/**','!node_modules/**'])
+        .pipe($.ghPages({
+            branch: 'master',
+            message : argv.m
+        }));
+});
+
+gulp.task('publish',['publish.coding.net','publish.github']);
