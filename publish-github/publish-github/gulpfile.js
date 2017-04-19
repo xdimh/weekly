1a89fdaa70aa1d7d005d5319839e8cb9c01aa813
xdimh <xdimhf@163.com>
2017-04-19 14:19:51 +0800

    Update 2017-04-19T06:19:46.838Z

--- publish-github/publish-github/gulpfile.js
+++ publish-github/publish-github/gulpfile.js
@@ -6,16 +6,38 @@
 
 'use strict';
 const gulp = require('gulp');
+const del = require('del');
 const gulpLoadPlugins = require('gulp-load-plugins');
 
 const $ = gulpLoadPlugins();
 
 // Publishes the site to GitHub Pages
-gulp.task('publish', () => {
+gulp.task('publish.github', () => {
     console.log('Publishing to GH Pages');
 return gulp.src(['./_book/**/*','CNAME'])
     .pipe($.ghPages({
+        cacheDir:'publish-github',
         origin: 'origin',
         branch: 'gh-pages'
     }));
+});
+
+
+// gulp.task('del',(cb) => {
+//     del(['.publish/**'], {dryRun: true}).then(paths => {
+//         console.log('Files and folders that would be deleted:\n', paths.join('\n'));
+//         cb();
+//     });
+// });
+
+// Publishes the site to GitHub Pages
+gulp.task('publish.coding.net', () => {
+    console.log('Publishing to coding.net Pages');
+    return gulp.src(['./_book/**/*'])
+        .pipe($.ghPages({
+            remoteUrl: 'https://git.coding.net/weekly/weekly.coding.me.git',
+            cacheDir : 'publish-coding-net',
+            branch: 'master',
+            force : true
+        }));
 });
